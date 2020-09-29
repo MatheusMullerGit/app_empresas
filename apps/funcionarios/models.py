a725ec98c39a62992540a1c47390dbfcd3a0b53e
MatheusMuller89 <matheusprofe@gmail.com>
2020-09-29 19:24:04 -0300

    Criando os relacionamentos de Bancos de Dados

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from apps.empresas.models import Empresa
 
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    empresas = models.ManyToManyField(Empresa)
     
     def __str__(self):
         return self.nome
