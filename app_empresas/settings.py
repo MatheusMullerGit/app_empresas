60a42ca3dd6ac773dea724abfb8a127713ecf85a
MatheusMuller89 <matheusprofe@gmail.com>
2020-10-03 17:51:13 -0300

    Adicionando Static Root

--- app_empresas/settings.py
+++ app_empresas/settings.py
@@ -95,15 +95,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles"),
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, "staticfiles"),
+#]
 
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 LOGIN_REDIRECT_URL = 'home'
 
